82701fa5d969318e45509a84699e430400288c60
MayorFresh <danielmayotar@gmail.com>
2022-12-19 14:42:47 +0100

    Updated the nodemailer
    it sends a confirmation email after signup but only works for a custom email. yet to make it select the user's email.

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -1,36 +1,52 @@
 const nodemailer = require("nodemailer");
+const SMTPPool = require("nodemailer/lib/smtp-pool");
 require('dotenv').config()
+const User = require('../model/user')
 
 const user = process.env.USER
 const pass = process.env.PASS
 
 const transport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: user,
-    pass: pass,
-  },
+    service: "gmail",
+    auth: {
+        user: user,
+        pass: pass,
+    },
 });
 
 
-const sendConfirmationEmail = (firstname, email, confirmationCode) => {
-    try{
-        // console.log("Check");
+
+const sendConfirmationEmail = async (req, res) => {
+    try {
+        // const {firstname, email, confirmationCode} = User(req.body)
+        // const email = User.email
+        // const firstname = User.firstname
+        // const confirmationCode = User.confirmationCode
         transport.sendMail({
-        from: user,
-        to: email,
-        subject: "Please confirm your account",
-        html: `<h1>Email Confirmation</h1>
-          <h2>Hello ${firstname}</h2>
-          <p>Welcome to NotesTakingApp. Please confirm your email by clicking on the following link</p>
-          <a href=http://localhost:4000/confirm/${confirmationCode}> Click here</a>
-          </div>`
-        })
+            from: user,
+            to: "dav33d88joh66nn99@gmail.com",
+            subject: "Please confirm your account",
+            html: `<h1>Email Confirmation</h1>
+              <h2>Hello ${User.firstname}</h2>
+              <p>Welcome to NotesTakingApp. Please confirm your email by clicking on the following link</p>
+              <a href=http://localhost:4000/confirm/${User.confirmationCode}> Click here</a>
+              </div>`
+            })
     }
-    catch(e) {
-        console.log(e);
-    } 
-  };
+    catch (e) {
+        console.log(e)
+    }
+}
+
+// transport.sendMail(sendConfirmationEmail, (e, info) => {
+//     if(e) {
+//         console.log(e)
+//     } else {
+//         console.log("Email sent " + info.response)
+//     }
+// })
+
+
 
 
   module.exports = {sendConfirmationEmail}
