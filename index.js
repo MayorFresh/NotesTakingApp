1ac1b2ef215bc7659902ea1213beb7297ca6188f
MayorFresh <danielmayotar@gmail.com>
2023-02-23 18:13:48 +0100

    changed the connection method

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ const limiter = rateLimiter({
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
+
 // apply helmet middleware for security
 app.use(helmet());
 app.use(limiter)
+app.use(cors());
 
 const swaggerUI = require('swagger-ui-express')
 const swaggerjsDoc = require('swagger-jsdoc')
@@ -26,7 +28,7 @@ const jsDoc = swaggerjsDoc(swaggerOption)
 
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(jsDoc))
 
-app.use(cors());
+
 
 //built-in middleware to parse requests
 app.use(express.json())
@@ -37,13 +39,18 @@ app.use('/api/v1/notesapp', user);
 
 // Server Connection
 const port = process.env.PORT;
-connectDB()
-.then(() => {
-    console.log(`Successfully Connected to the database`)
-    app.listen (port, () => {
-        console.log("Server is listening on Port " + port);
-    });
-})
-.catch((error) => {
-    console.log('Could not connect to the database\n', error)
-})
+
+const start = async () => {
+    try {
+        await connectDB()
+        console.log(`Successfully Connected to the database`)
+        app.listen (port, () => {
+            console.log("Server is listening on Port " + port);
+        });
+    }
+    catch (e) {
+        console.log('Could not connect to the database\n', e)
+    }
+}
+
+start();
